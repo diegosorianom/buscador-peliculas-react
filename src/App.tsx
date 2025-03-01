79db9e1bf63410c200cbb4d2ebe0076dfc548856
diego <diegosorianomagallon@gmail.com>
2025-03-01 15:21:39 +0100

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style = {{ textAlign: "center", marginTop: "50px"}} >
+      <h1>Hola mundo</h1>
+      <p>Página en Github Pages usando React.</p>
+    </div>
+  )
+}
+
+export default App;
